a3240ba983e65a23757ab9440cff8e9428d52ae4
parkjun-0521 <parkjun11114@gmail.com>
2024-07-14 18:16:11 +0900

    feat : 회원가입 완성
    회원가입 기능 완성
    DB에 실제 유저를 만들고 로그인 까지 구현

--- Assets/02.Scripts/02.Manager/LoginManager.cs
+++ Assets/02.Scripts/02.Manager/LoginManager.cs
@@ -6,20 +6,31 @@ using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
+    // 로그인 
     public InputField idInput;
     public InputField passwordInput;
     public Button loginButton;
+
+    // 회원가입
+    public InputField createUserIDInput;
+    public InputField createUserPWInput;
+    public Button createUserButton;
+
     public Text StatusText;
 
     string LoginURL = "http://localhost/Zombie_Login.php";
+    string CreateUserURL = "http://localhost/Zombie_NewCreateUser.php";
     //string LoginURL = "http://223.131.75.181:1356//Zombie_Login.php";
+    //string CreateUserURL = "http://223.131.75.181:1356/Zombie_NewCreateUser.php";
 
     void Start() {
         NetworkManager.Instance.NickNameInput = idInput;
         NetworkManager.Instance.statusText = StatusText;
         loginButton.onClick.AddListener(OnLogin);
+        createUserButton.onClick.AddListener(OnCreateUser);
     }
 
+    // 로그인 
     void OnLogin() {
         if (idInput.text.Length > 0 && passwordInput.text.Length > 0) {
             StartCoroutine(LoginToDB(idInput.text, passwordInput.text));
@@ -28,7 +39,6 @@ public class LoginManager : MonoBehaviour
             Debug.Log("아이디 또는 패스워드를 입력해주세요!");
         }
     }
-
     IEnumerator LoginToDB(string username, string password)
     {
         WWWForm form = new WWWForm();
@@ -53,5 +63,32 @@ public class LoginManager : MonoBehaviour
                 }
             }
         }
+    }
+
+    // 회원가입
+    void OnCreateUser()
+    {
+        if (createUserIDInput.text.Length > 0 && createUserPWInput.text.Length > 0) {
+            StartCoroutine(NewCreateUser(createUserIDInput.text, createUserPWInput.text));
+        }
+        else {
+            Debug.Log("아이디 또는 패스워드를 입력해주세요!");
+        }
+    }
+    IEnumerator NewCreateUser(string  username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("UserID", username);
+        form.AddField("UserPassword", password);
+        using (UnityWebRequest www = UnityWebRequest.Post(CreateUserURL, form)) {
+            yield return www.SendWebRequest();
+
+            if(www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError) {
+                Debug.Log("회원이 만들어지지 않음");
+            }
+            else {
+                Debug.Log("회원가입 완료");
+            }
+        } 
     }
 }
