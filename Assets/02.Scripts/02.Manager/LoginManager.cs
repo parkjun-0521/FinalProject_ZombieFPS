4ca473036242e03eb9f03f54b54e45bd182df63e
parkjun-0521 <parkjun11114@gmail.com>
2024-07-14 17:54:18 +0900

    feat : 로그인 DB 연결 완료
    PHP를 사용하여 유니티와 mysql의 연결 완료
    로그인 정보를 받아와 로그인을 할 수 있다.

--- Assets/02.Scripts/02.Manager/LoginManager.cs
+++ Assets/02.Scripts/02.Manager/LoginManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
@@ -10,6 +11,9 @@ public class LoginManager : MonoBehaviour
     public Button loginButton;
     public Text StatusText;
 
+    string LoginURL = "http://localhost/Zombie_Login.php";
+    //string LoginURL = "http://223.131.75.181:1356//Zombie_Login.php";
+
     void Start() {
         NetworkManager.Instance.NickNameInput = idInput;
         NetworkManager.Instance.statusText = StatusText;
@@ -17,8 +21,37 @@ public class LoginManager : MonoBehaviour
     }
 
     void OnLogin() {
-        if (idInput.text.Length > 0 && passwordInput.text.Length > 0) {          
-            NetworkManager.Instance.Connect();
+        if (idInput.text.Length > 0 && passwordInput.text.Length > 0) {
+            StartCoroutine(LoginToDB(idInput.text, passwordInput.text));
+        }
+        else {
+            Debug.Log("아이디 또는 패스워드를 입력해주세요!");
+        }
+    }
+
+    IEnumerator LoginToDB(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("UserID", username);
+        form.AddField("UserPassword", password);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(LoginURL, form)) {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError) {
+                StatusText.text = "Error connecting to server: " + www.error;
+                Debug.LogError(www.error);
+            }
+            else {
+                if (www.downloadHandler.text == "1") // 로그인 성공
+                {
+                    NetworkManager.Instance.Connect();
+                }
+                else // 로그인 실패
+                {
+                    Debug.Log("아이디 또는 비밀번호가 틀렸습니다");
+                }
+            }
         }
     }
 }
