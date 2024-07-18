9e1713660c3d6cfeef0dc9448034cd9ac322201c
DESKTOP-7FL9E6G\User <parkjun436@gmail.com>
2024-07-18 16:54:31 +0900

    feat : 엘리트 좀비 에셋

---
+++ Assets/02.Scripts/01.Network/URLs.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public static class URLs {
+    /*public const string LoginURL = "http://localhost/Zombie_Login.php";
+    public const string CreateUserURL = "http://localhost/Zombie_NewCreateUser.php"; */
+    public const string LoginURL = "http://223.131.75.181:1356//Zombie_Login.php";
+    public const string CreateUserURL = "http://223.131.75.181:1356/Zombie_NewCreateUser.php";
+}
