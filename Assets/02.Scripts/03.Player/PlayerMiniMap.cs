a9d914e2fd6095bb3d7912276ee8ead4337ece8f
house <osj0004@naver.com>
2024-07-28 21:16:10 +0900

    feat : 미니맵추가

--- Assets/02.Scripts/03.Player/PlayerMiniMap.cs
+++ Assets/02.Scripts/03.Player/PlayerMiniMap.cs
@@ -1,107 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class MyRectTransform
-{
-    private RectTransform rectTransform;
-
-    // 생성자: RectTransform을 받아서 MyRectTransform을 초기화합니다.
-    public MyRectTransform(RectTransform rt)
-    {
-        rectTransform = rt;
-    }
-
-    // Left: offsetMin.x와 연결
-    public float Left
-    {
-        get { return rectTransform.offsetMin.x; }
-        set
-        {
-            Vector2 offset = rectTransform.offsetMin;
-            offset.x = value;
-            rectTransform.offsetMin = offset;
-        }
-    }
-
-    // Right: offsetMax.x와 연결
-    public float Right
-    {
-        get { return -rectTransform.offsetMax.x; }
-        set
-        {
-            Vector2 offset = rectTransform.offsetMax;
-            offset.x = -value;
-            rectTransform.offsetMax = offset;
-        }
-    }
-
-    // Top: offsetMax.y와 연결
-    public float Top
-    {
-        get { return -rectTransform.offsetMax.y; }
-        set
-        {
-            Vector2 offset = rectTransform.offsetMax;
-            offset.y = -value;
-            rectTransform.offsetMax = offset;
-        }
-    }
-
-    // Bottom: offsetMin.y와 연결
-    public float Bottom
-    {
-        get { return rectTransform.offsetMin.y; }
-        set
-        {
-            Vector2 offset = rectTransform.offsetMin;
-            offset.y = value;
-            rectTransform.offsetMin = offset;
-        }
-    }
-}
 public class PlayerMiniMap : MonoBehaviour
 {
-    public GameObject MiniMap;
+    //public GameObject MiniMap;
     public RectTransform minimap;
+    public GameObject playerUI;
     Player player;
+    [SerializeField]GameObject[] players;
+    [SerializeField] float sizeX = 8;
+    [SerializeField] float sizeY = 8f;
+    [SerializeField] Vector3 temp = new Vector3(-35.26f, 0 ,1.05f);
+    [SerializeField] GameObject[] playersIcon;
+    [SerializeField] RectTransform[] playerIconRect;
+    [SerializeField] Vector3 otherTemp = new Vector3(36.08f, 0, -1.1f);
+    [SerializeField] float otherPlayerSize = 1.35f;
+    bool isPlayerFind = true;
 
-    Vector3 prevPlayerPos;
-    Vector3 playerPos;
-    Vector3 PlayerPos
+    int playerCount = 0;
+    int PlayerCount
     {
         get
         {
-            return playerPos;
+            return playerCount;
         }
         set
         {
-            prevPlayerPos = value;
-            playerPos += value;
-           // Debug.Log(value.magnitude);
+            if (playerCount == PhotonNetwork.CurrentRoom.PlayerCount) return;
+            playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+            StartCoroutine(Search());
+            Debug.Log("프로퍼티작동");
         }
     }
+    GameObject otherPlayerUI;
 
-    
     private void Awake()
     {
-        
+
     }
     private void Start()
     {
-        MyRectTransform myRect = new MyRectTransform(minimap);
         player = GameObject.FindWithTag("Player").GetComponent<Player>();
-        
     }
     private void Update()
     {
         if (player.PV.IsMine)
         {
-            PlayerPos = player.transform.position;
+            PlayerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+            minimap.offsetMax = new Vector2((temp.x + player.transform.position.x) * sizeX, (temp.z + player.transform.position.z) * sizeY);
+            minimap.offsetMin = new Vector2((temp.x + player.transform.position.x) * sizeX, (temp.z + player.transform.position.z) * sizeY);
+            
 
-            Debug.Log("미니맵로컬포지션" + minimap.localPosition);
-            Debug.Log("미니맵tran포지션" + minimap.transform.position);
-            Debug.Log("미니맵포지션" + minimap.position);
+            if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
+            {
+                OtherPlayerIcon(2);
+            }
+            else if(PhotonNetwork.CurrentRoom.PlayerCount == 3)
+            {
+                OtherPlayerIcon(3);
+            }
+            else if (PhotonNetwork.CurrentRoom.PlayerCount == 4)
+            {
+                OtherPlayerIcon(4);
+            }
         }
+    }
+
+    void OtherPlayerIcon(float num)
+    {
+        switch (num)
+        {
+            case 2:
+                playersIcon[0].SetActive(true);
+                playerIconRect[0].offsetMax = new Vector2((otherTemp.x - players[1].transform.position.x) * otherPlayerSize, (otherTemp.z - players[1].transform.position.z) * otherPlayerSize);
+                playerIconRect[0].offsetMin = new Vector2((otherTemp.x - players[1].transform.position.x) * otherPlayerSize, (otherTemp.z - players[1].transform.position.z) * otherPlayerSize);
+                break;
+            case 3:
+                playersIcon[0].SetActive(true);
+                playerIconRect[0].offsetMax = new Vector2((otherTemp.x - players[1].transform.position.x) * otherPlayerSize, (otherTemp.z - players[1].transform.position.z) * otherPlayerSize);
+                playerIconRect[0].offsetMin = new Vector2((otherTemp.x - players[1].transform.position.x) * otherPlayerSize, (otherTemp.z - players[1].transform.position.z) * otherPlayerSize);
+
+                playersIcon[1].SetActive(true);
+                playerIconRect[1].offsetMax = new Vector2((otherTemp.x - players[2].transform.position.x) * otherPlayerSize, (otherTemp.z - players[2].transform.position.z) * otherPlayerSize);
+                playerIconRect[1].offsetMin = new Vector2((otherTemp.x - players[2].transform.position.x) * otherPlayerSize, (otherTemp.z - players[2].transform.position.z) * otherPlayerSize);
+                break;
+            case 4:
+                playersIcon[0].SetActive(true);
+                playerIconRect[0].offsetMax = new Vector2((otherTemp.x - players[1].transform.position.x) * otherPlayerSize, (otherTemp.z - players[1].transform.position.z) * otherPlayerSize);
+                playerIconRect[0].offsetMin = new Vector2((otherTemp.x - players[1].transform.position.x) * otherPlayerSize, (otherTemp.z - players[1].transform.position.z) * otherPlayerSize);
+
+                playersIcon[1].SetActive(true);
+                playerIconRect[1].offsetMax = new Vector2((otherTemp.x - players[2].transform.position.x) * otherPlayerSize, (otherTemp.z - players[2].transform.position.z) * otherPlayerSize);
+                playerIconRect[1].offsetMin = new Vector2((otherTemp.x - players[2].transform.position.x) * otherPlayerSize, (otherTemp.z - players[2].transform.position.z) * otherPlayerSize);
+
+                playersIcon[2].SetActive(true);
+                playerIconRect[2].offsetMax = new Vector2((otherTemp.x - players[3].transform.position.x) * otherPlayerSize, (otherTemp.z - players[3].transform.position.z) * otherPlayerSize);
+                playerIconRect[2].offsetMin = new Vector2((otherTemp.x - players[3].transform.position.x) * otherPlayerSize, (otherTemp.z - players[3].transform.position.z) * otherPlayerSize);
+                break;
+        }
+    }
+
+    IEnumerator Search()
+    {
+        yield return new WaitForSeconds(1.0f);
+        players = GameObject.FindGameObjectsWithTag("Player");
     }
 }
