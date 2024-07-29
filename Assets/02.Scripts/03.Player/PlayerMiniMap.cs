c42287a1a87754105daf703fdcdb4eff31ca3119
DESKTOP-7FL9E6G\User <osj0004@naver.com>
2024-07-29 14:03:36 +0900

    fix : 미니맵수정

--- Assets/02.Scripts/03.Player/PlayerMiniMap.cs
+++ Assets/02.Scripts/03.Player/PlayerMiniMap.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class PlayerMiniMap : MonoBehaviour
+public class PlayerMiniMap : MonoBehaviourPun
 {
     //public GameObject MiniMap;
     public RectTransform minimap;
@@ -15,10 +15,11 @@ public class PlayerMiniMap : MonoBehaviour
     [SerializeField] Vector3 temp = new Vector3(-35.26f, 0 ,1.05f);
     [SerializeField] GameObject[] playersIcon;
     [SerializeField] RectTransform[] playerIconRect;
-    [SerializeField] Vector3 otherTemp = new Vector3(36.08f, 0, -1.1f);
+    [SerializeField] Vector3 otherTemp = new Vector3(36.08f, 0, -0.31f);
     [SerializeField] float otherPlayerSize = 1.35f;
     bool isPlayerFind = true;
 
+    
     int playerCount = 0;
     int PlayerCount
     {
@@ -46,14 +47,18 @@ public class PlayerMiniMap : MonoBehaviour
     }
     private void Update()
     {
-        if (player.PV.IsMine)
+        if (photonView.IsMine)
         {
             PlayerCount = PhotonNetwork.CurrentRoom.PlayerCount;
             minimap.offsetMax = new Vector2((temp.x + player.transform.position.x) * sizeX, (temp.z + player.transform.position.z) * sizeY);
             minimap.offsetMin = new Vector2((temp.x + player.transform.position.x) * sizeX, (temp.z + player.transform.position.z) * sizeY);
-            
 
-            if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
+
+            if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                OtherPlayerIcon(1);
+            }
+            else if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
             {
                 OtherPlayerIcon(2);
             }
@@ -70,32 +75,43 @@ public class PlayerMiniMap : MonoBehaviour
 
     void OtherPlayerIcon(float num)
     {
+        for(int i = 0; i < num - 1; i++)
+        {
+            playersIcon[i].SetActive(true);
+        }
+        for(int i = 2; i > num - 2; i--)  //1     2  0    3 1    4   2
+        {
+            playersIcon[i].SetActive(false);
+        }
         switch (num)
         {
             case 2:
-                playersIcon[0].SetActive(true);
+                //playersIcon[0].SetActive(true);
+                if (players.Length != 2) return;
                 playerIconRect[0].offsetMax = new Vector2((otherTemp.x - players[1].transform.position.x) * otherPlayerSize, (otherTemp.z - players[1].transform.position.z) * otherPlayerSize);
                 playerIconRect[0].offsetMin = new Vector2((otherTemp.x - players[1].transform.position.x) * otherPlayerSize, (otherTemp.z - players[1].transform.position.z) * otherPlayerSize);
                 break;
             case 3:
-                playersIcon[0].SetActive(true);
+                //playersIcon[0].SetActive(true);
+                if (players.Length != 3) return;
                 playerIconRect[0].offsetMax = new Vector2((otherTemp.x - players[1].transform.position.x) * otherPlayerSize, (otherTemp.z - players[1].transform.position.z) * otherPlayerSize);
                 playerIconRect[0].offsetMin = new Vector2((otherTemp.x - players[1].transform.position.x) * otherPlayerSize, (otherTemp.z - players[1].transform.position.z) * otherPlayerSize);
 
-                playersIcon[1].SetActive(true);
+                //playersIcon[1].SetActive(true);
                 playerIconRect[1].offsetMax = new Vector2((otherTemp.x - players[2].transform.position.x) * otherPlayerSize, (otherTemp.z - players[2].transform.position.z) * otherPlayerSize);
                 playerIconRect[1].offsetMin = new Vector2((otherTemp.x - players[2].transform.position.x) * otherPlayerSize, (otherTemp.z - players[2].transform.position.z) * otherPlayerSize);
                 break;
             case 4:
-                playersIcon[0].SetActive(true);
+                //playersIcon[0].SetActive(true);
+                if (players.Length != 4) return;
                 playerIconRect[0].offsetMax = new Vector2((otherTemp.x - players[1].transform.position.x) * otherPlayerSize, (otherTemp.z - players[1].transform.position.z) * otherPlayerSize);
                 playerIconRect[0].offsetMin = new Vector2((otherTemp.x - players[1].transform.position.x) * otherPlayerSize, (otherTemp.z - players[1].transform.position.z) * otherPlayerSize);
 
-                playersIcon[1].SetActive(true);
+                //playersIcon[1].SetActive(true);
                 playerIconRect[1].offsetMax = new Vector2((otherTemp.x - players[2].transform.position.x) * otherPlayerSize, (otherTemp.z - players[2].transform.position.z) * otherPlayerSize);
                 playerIconRect[1].offsetMin = new Vector2((otherTemp.x - players[2].transform.position.x) * otherPlayerSize, (otherTemp.z - players[2].transform.position.z) * otherPlayerSize);
 
-                playersIcon[2].SetActive(true);
+                //playersIcon[2].SetActive(true);
                 playerIconRect[2].offsetMax = new Vector2((otherTemp.x - players[3].transform.position.x) * otherPlayerSize, (otherTemp.z - players[3].transform.position.z) * otherPlayerSize);
                 playerIconRect[2].offsetMin = new Vector2((otherTemp.x - players[3].transform.position.x) * otherPlayerSize, (otherTemp.z - players[3].transform.position.z) * otherPlayerSize);
                 break;
--- Assets/02.Scripts/05.Item/ItemSupportGrenade.cs
+++ Assets/02.Scripts/05.Item/ItemSupportGrenade.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 public class ItemSupportGrenade : MonoBehaviour
 {
     public ItemController itemData;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            gameObject.SetActive(false);
+            //PhotonNetwork.Instantiate("ЦјАн", transform.position, Quaternion.identity);
+        }
+    }
 }
