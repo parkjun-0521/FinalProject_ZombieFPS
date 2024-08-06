74ea024e2dae4056428e0ab4d2dcdc8a552375c1
DESKTOP-7FL9E6G\User <parkjun436@gmail.com>
2024-08-06 12:23:01 +0900

    feat : 몬스터 무한 스포너 완성
    무한 스포너에서 생성된 좀비는 탐지 반경을 100으로 하여 플레이어를 바로 찾아서 올 수 있도록 함

--- Assets/02.Scripts/02.Manager/EnemySpawnManager.cs
+++ Assets/02.Scripts/02.Manager/EnemySpawnManager.cs
@@ -11,39 +11,61 @@ public class EnemySpawnManager : MonoBehaviourPun
     public bool isSpawn;
     public int spawnCount;          // 생성되는 좀비 숫자 
     GameObject enemyObj;            // 좀비 오브젝트 
+    public bool isInfinite;         // 무한 생성 트리거 
+    public float spawnInterval = 1f; // 좀비 생성 간격 시간 (초 단위)
 
     // 좀비 이름 
     public string[] enemyName = { "Zombie1", "EliteMeleeZombie", "EliteRangeZombie", "BossZombie", "Boss_Phobos" };
     public int[] persent;           // 각 좀비의 등장 확률 
-        
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player") && !isSpawn)
-        {
-            isSpawn = true;
-            photonView.RPC("SpawnEnemies", RpcTarget.AllBuffered, isSpawn);
 
-            // 개수 만큼 좀비 생성 
-            for (int i = 0; i < spawnCount; i++) {
-                int randomIndex = Random.Range(0, 100);
-                int cumulativePercentage = 0;
-
-                // 좀비 등장확률 
-                for (int j = 0; j < enemyName.Length; j++) {
-                    cumulativePercentage += persent[j];
-                    if (randomIndex < cumulativePercentage) {
-                        enemyObj = Pooling.instance.GetObject(enemyName[j], spawnPoint.position);
-                        break;
-                    }
-                }
-                if(enemyObj.transform != null)
-                    enemyObj.transform.rotation = transform.rotation;
-                EnemyController enemyLogin = enemyObj.GetComponent<EnemyController>();
-                enemyLogin.enemySpawn = spawnPoint;
+    private void OnTriggerEnter( Collider other ) {
+        if (other.gameObject.CompareTag("Player") && !isSpawn) {
+            if (!isInfinite) {
+                EnemySpawn();
+            }
+            else {
+                StartCoroutine(InfiniteEnemySpawn());
             }
         }
     }
 
+    IEnumerator InfiniteEnemySpawn() {
+        while (true) {
+            EnemySpawn();
+            yield return new WaitForSeconds(spawnInterval); // 지정된 시간 간격으로 대기
+        }
+    }
+
+
+
+    public void EnemySpawn() {
+        isSpawn = true;
+        photonView.RPC("SpawnEnemies", RpcTarget.AllBuffered, isSpawn);
+
+        // 개수 만큼 좀비 생성 
+        for (int i = 0; i < spawnCount; i++) {
+            int randomIndex = Random.Range(0, 100);
+            int cumulativePercentage = 0;
+
+            // 좀비 등장확률 
+            for (int j = 0; j < enemyName.Length; j++) {
+                cumulativePercentage += persent[j];
+                if (randomIndex < cumulativePercentage) {
+                    enemyObj = Pooling.instance.GetObject(enemyName[j], spawnPoint.position);
+                    break;
+                }
+            }
+            if (enemyObj.transform != null)
+                enemyObj.transform.rotation = transform.rotation;
+
+
+            EnemyController enemyLogin = enemyObj.GetComponent<EnemyController>();
+            enemyLogin.enemySpawn = spawnPoint;
+            if (isInfinite) {
+                enemyLogin.EnemyLookRange.radius = 100;
+            }
+        }
+    }
 
     [PunRPC]
     void SpawnEnemies(bool isSpawn) {
