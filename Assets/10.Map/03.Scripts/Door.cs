0c202f0f2c6c9159f4012bf32ac4a247c270c859
DESKTOP-7FL9E6G\User <parkjun436@gmail.com>
2024-08-06 12:05:44 +0900

    feat : 3스테이지 스폰 포인트 및 2스테이지 다음 스폰 지점 UI 개선

--- Assets/10.Map/03.Scripts/Door.cs
+++ Assets/10.Map/03.Scripts/Door.cs
@@ -5,31 +5,30 @@ public class Door : MonoBehaviourPun
 {
     private Animator animator;
     public bool isOpen = false;
-
-    void Start()
-    {
+    [SerializeField] GameObject otherObject;
+    void Start() {
+        if (otherObject != null) {
+            animator = otherObject.GetComponent<Animator>();
+            return;
+        }
         animator = GetComponent<Animator>();
     }
 
-    public void OpenDoorRPC()
-    {
+    public void OpenDoorRPC() {
         photonView.RPC("OpenDoor", RpcTarget.AllBuffered);
     }
     [PunRPC]
-    public void OpenDoor()
-    {
+    public void OpenDoor() {
         animator.SetBool("isOpen", true);
         isOpen = true;
     }
 
 
-    public void CloseDoorRPC()
-    {
+    public void CloseDoorRPC() {
         photonView.RPC("CloseDoor", RpcTarget.AllBuffered);
     }
     [PunRPC]
-    public void CloseDoor()
-    {
+    public void CloseDoor() {
         animator.SetBool("isOpen", false);
         isOpen = false;
     }
